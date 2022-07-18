b04fdf4ccddd1ec122e912c83842962d03253d85
Sundeep Agarwal <learnbyexample.net@gmail.com>
2022-07-18 13:58:59 +0530

    updated shell scripts

---
+++ example_files/shell_scripting/comments.sh
@@ -0,0 +1,2 @@
+# this is a comment on its own line
+echo 'hello' # and this is a comment after a command
--- example_files/shell_scripting/if_then_else.sh
+++ example_files/shell_scripting/if_then_else.sh
@@ -3,7 +3,7 @@ if (( $# != 1 )) ; then
     exit 1
 else
     if [[ ! -f $1 ]] ; then
-        echo "Error! '$1' is not a valid file" 1>&2
+        printf 'Error! %q is not a valid file\n' "$1" 1>&2
         exit 1
     else
         echo "No. of lines in '$1' is $(wc -l < "$1")"
